24f982dad778ee4a40209f7745f544c845ca580d
Vladislav <lenvladik@gmail.com>
2016-08-31 13:37:28 +0300

    hm3

--- hm/src/module3/task3/Solution.java
+++ hm/src/module3/task3/Solution.java
@@ -9,11 +9,11 @@ public class Solution {
     Course Course5 = new Course(new Date(7428263382468L),"Sonya");
     Student Student1 = new Student("Vlad","Len'",2);
     Student Student2 = new Student("Oly",new Course[3]);
-    CollegeStudent Student3 = new CollegeStudent("KKTE", 7, 975453);
+    CollegeStudent Student3 = new CollegeStudent("Bob", "Dealan", 3,"KKTE", 7, 975453);
     CollegeStudent Student4 = new CollegeStudent("Wein",new Course[3]);
     CollegeStudent Student5 = new CollegeStudent("Naoko","Oonishi",2);
-    SpecialStudent Student6 = new SpecialStudent(347235, "key@mail");
-    SpecialStudent Student7 = new SpecialStudent("KNEK", 8, 987673);
+    SpecialStudent Student6 = new SpecialStudent("Naoko","Oonishi",2,347235, "key@mail");
+    SpecialStudent Student7 = new SpecialStudent("Creig",new Course[1]);
     SpecialStudent Student8 = new SpecialStudent("Diego", "Campos", 2);
 }
 
