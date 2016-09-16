883990161c00c3021fe3fc061f6db50b72dd0d3f
Vladislav <lenvladik@gmail.com>
2016-09-17 01:22:24 +0300

    hm5

--- hm/src/module5/DAOImpl.java
+++ hm/src/module5/DAOImpl.java
@@ -6,55 +6,53 @@ public class DAOImpl implements DAO {
     Room [] db = new Room [10];
     @Override
     public Room save(Room room) {
-        int i =0;
-        int count = 0;
-        for (int p = 0; p < db.length; p++) {
-            if (db[p] != null)
-               count++ ;}
-               if (count == db.length)
-                   System.out.println("Data Base is full.");
-        else for (i = 0; i < db.length; i++) {
+        for (int i = 0; i < db.length; i++) {
             if (db[i] == null) {
-                db[i] = room;
                 System.out.println(room + " was saved.");
                 return db[i];
-                    }
-                }
-
-        return db[i];
+            }
+        }
+        System.out.println("Data Base is full.");
+        return null;
     }
 
     @Override
     public boolean delete(Room room) {
-        for (int i = 0; i<db.length;i++){
-            if (room == db[i]){
+        for (int i = 0; i<db.length;i++) {
+            if (room == db[i]) {
                 db[i] = null;
-                break;}}
-        System.out.println(room + " was deleted.");
-        return true;
+                System.out.println(room + " was deleted.");
+                return true;
+            }
+        }
+        System.out.println(room + " was not found.");
+        return false;
     }
 
     @Override
     public Room update(Room room) {
         int i =0;
-        for (i = 0; i < db.length;i++){
-            if (room.getId() == db[i].getId()){
+        for (i = 0; i < db.length; i++) {
+            if (room.getId() == db[i].getId()) {
                 db[i] = room;
                 System.out.println(room + " was updated.");
-                break;}}
-
-        return db[i];
+                return db[i];
+            }
+        }
+        System.out.println(room + " was not found.");
+        return null;
     }
 
     @Override
     public Room findById(long id) {
-        int i;
-       for ( i = 0; i < db.length; i++){
-            if (db[i].getId() == id ){
-                System.out.println(db[i]+" is found.");
-            return db[i];}
+        for (int i = 0; i < db.length; i++) {
+            if (db[i].getId() == id ) {
+                System.out.println(db[i] + " is found.");
+                return db[i];
+            }
         }
-        return db[i];
+        System.out.println("Id was not found.");
+        return null;
     }
 
     @Override
